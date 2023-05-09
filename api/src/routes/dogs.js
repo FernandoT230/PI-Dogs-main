04b9850e7f42ea856d57ce290d178d29708222cd
FernandoT230 <fernando712torres@gmail.com>
2023-05-09 12:00:10 -0400

    re-modularized

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,26 +1,32 @@
 const { Router } = require("express");
 const axios = require("axios");
 const { Dog, Temperament } = require("../db");
-const { getAllDogs } = require('./controllers');
+const { getAllDogs } = require('../controllers/controllers');
 
 const router = Router();
 
 //Endpoint para obtener todos los perros
 router.get("/", async (req, res) => {
-  const name = req.query.name;
-  let dogsTotal = await getAllDogs();
-  if (name) {
-    let dogName = await dogsTotal.filter((dog) =>
-      dog.name.toLowerCase().includes(name.toLowerCase())
-    );
-    dogName.length
-      ? res.status(200).send(dogName)
-      : res
-          .status(404)
-          .send("Cann't find the dog with the name you are looking for");
-  } else {
-    res.status(200).send(dogsTotal);
-  }
-});
+    try {
+      const name = req.query.name;
+      let dogsTotal = await getAllDogs();
+      if (name) {
+        let dogName = await dogsTotal.filter((dog) =>
+          dog.name.toLowerCase().includes(name.toLowerCase())
+        );
+        dogName.length
+          ? res.status(200).send(dogName)
+          : res
+              .status(404)
+              .send("Cann't find the dog with the name you are looking for");
+      } else {
+        res.status(200).send(dogsTotal);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal server error");
+    }
+  });
+  
 
 module.exports = router;
--- api/src/routes/temperament.js
+++ api/src/routes/temperament.js
@@ -5,22 +5,27 @@ const { Temperament } = require("../db");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const temperamentApi = await axios.get(
-    "https://api.thedogapi.com/v1/breeds"
-  );
-  const temperament = temperamentApi.data
-    .map((dog) => (dog.temperament ? dog.temperament : "No info"))
-    .map((dog) => dog.split(", "));
-  let eachTemperament = [...new Set(temperament.flat())];
-  eachTemperament.forEach(async (el) => {
-    if (el) {
-      await Temperament.findOrCreate({
-        where: { name: el },
+    try {
+      const temperamentApi = await axios.get(
+        "https://api.thedogapi.com/v1/breeds"
+      );
+      const temperament = temperamentApi.data
+        .map((dog) => (dog.temperament ? dog.temperament : "No info"))
+        .map((dog) => dog.split(", "));
+      let eachTemperament = [...new Set(temperament.flat())];
+      eachTemperament.forEach(async (el) => {
+        if (el) {
+          await Temperament.findOrCreate({
+            where: { name: el },
+          });
+        }
       });
+      const allTemperaments = await Temperament.findAll();
+      res.send(allTemperaments);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal server error");
     }
   });
-  const allTemperaments = await Temperament.findAll();
-  res.send(allTemperaments);
-});
 
 module.exports = router;
